bb2cc8b1bc5ff4337c495d469d19dec286b1c12f
ritualcode <ritualcode@gmail.com>
2015-04-23 12:31:18 +0300

    Application April 23

---
+++ js/router.js
@@ -0,0 +1,27 @@
+﻿var router = {
+	routes: {
+		register: {
+			html: "register.html"
+		},
+		login: {
+			html: "login.html"
+		}
+	},
+	run: function() {
+		var $container = $(".main");
+		var url = location.hash.slice(1);
+		if (url != "") {
+			var rout = router.routes[url];
+			$container.load(rout.html);
+		} else {
+			$container.load("start.html");
+		}
+	},
+	bindListeners: function() {
+		$(window).on("hashchange", router.run);
+		window.addEventListener('load', router.run);
+	},
+	init: function () {
+		this.bindListeners();
+	}
+}
---
+++ js/script.js
@@ -0,0 +1,3 @@
+﻿$(function () {
+	router.init();
+});
